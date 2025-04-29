6d5efbadc24b782c4e932485823fb419287f039f
HelaGone <luah@gmail.com>
2025-04-28 20:52:52 -0600

    fix styles

--- humedales/app/components/atoms/heading.jsx
+++ humedales/app/components/atoms/heading.jsx
@@ -1,14 +1,14 @@
-import style from "./heading.module.css"
+import styles from "./heading.module.css"
 export default function heading(props){
 
     const texto = props.texto;
     const orient = props.orientacion;
-    const orientation = orient === "vertical" ? "vertical-rl" : "unset";
-    const style = {writingMode: orientation}
+    const orientation = orient === "vertical" ? "vertical-lr" : "unset";
+    const inlineStyle = {writingMode: orientation}
+    // El problema era un typo en la palabra style. Debe ser style, no styles
     return(
-        <h1 className={style.titulo_prin} styles={{"writingMode":orientation}}>
+        <h1 className={styles.titulo_prin} style={inlineStyle}>
             {texto}
         </h1>
-
     );
 }
