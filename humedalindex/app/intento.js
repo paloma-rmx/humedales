0d152d356525a7548dbf37ed5bf785fc72be2bcf
Paloma Rementeria <palomarmx@Palomas-MacBook-Pro.local>
2025-05-14 20:48:03 -0600

    estilos tarjeta

---
+++ humedalindex/app/intento.js
@@ -0,0 +1,15 @@
+import styles from "./page.module.css";
+import Heading from "./components/atoms/heading";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      {/* Acá va el contenido principal html */}
+      <section className={styles.section}>
+        <figure className={styles.figure}>
+    
+        </figure>
+      </section>
+    </main>
+  );
+}
--- humedalindex/app/page.js
+++ humedalindex/app/page.js
@@ -9,19 +9,22 @@ export default function Home() {
         <figure className={styles.figure}>
           <img src="https://cimanorte.com/wp-content/uploads/swallow-3439543_1920.jpg" alt="Golorondrina" className={styles.image} />
           <figcaption className={styles.figcaption}>
-            <span className={styles.ide}>1</span>
-            <h2 className={styles.figcaption_title}>Hirundo rustica</h2>
+            <div className={styles.caption_container}>
+              <span className={styles.ide}>1</span>
+              <h2 className={styles.figcaption_title}>Hirundo rustica</h2>
+            </div>
             <h3 className={styles.figcaption_subtitle}>Golondrina tijereta</h3>
             <p className={styles.figcaption_text}>
             Golondrina común o andorina es una especie de ave paseriforme de la familia Hirundinidae de hábitos migratorios. Se trata de la golondrina con la mayor área de distribución del mundo. Habita en Europa, Asia, África, América y parte de Australasia. En México se le encuentra en la mayor parte de su territorio. Su alimentación es principlamente de insectos y frutos. 
             </p>
             <div className={styles.figcaption_info}>
-              <p className={styles.left_list}>animales. 
-cordados. 
-aves. 
-aves de percha. 
-golondrinas. 
-golondrinas típicas.</p>
+              <ul className={styles.left_list}>
+                <li>animales.</li> 
+                <li>cordados. </li>
+                <li>aves. </li>
+                <li>aves de percha. </li>
+                <li>golondrinas. </li>
+                <li>golondrinas típicas.</li> </ul>
               <ul className={styles.right_list}>
                 <li>aves</li>
                 <li>riesgo menor</li>
